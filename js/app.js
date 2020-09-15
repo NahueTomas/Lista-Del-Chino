9701d6258ef989b8c2e4ccbb4a6d5e0b5234bcf6
Nahuel <69236339+NahueTomas@users.noreply.github.com>
2020-09-14 23:03:49 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,77 +1,65 @@
 const form  = document.querySelector('form')
 const list = document.querySelector('.list')
-
 let arrayList = []
-
 main()
-
 function main(){
     document.addEventListener('DOMContentLoaded', readLS)
     form.addEventListener('submit', saveText)
     list.addEventListener('click', deleteItem)
 }
-
 function saveText(e){
     e.preventDefault()
     text = e.target.children[0].value
     if (text.length > 1 ){
-        textObj = {
-            id: Date.now(),
-            text
-        }
+        textObj = {id: Date.now(), status: false, text}
         arrayList = [...arrayList, textObj]
         form.reset()
         createItem(arrayList)
     }
 }
-
 function createItem(value){
     clearHTML()
     value.forEach(element => {
         const item = document.createElement('li')
-
-        const btnC = document.createElement('input')
-        btnC.setAttribute('type', 'checkbox')
-
-        const btnD = document.createElement('img')
-        btnD.setAttribute('src', 'img/trash-icon.png')
-        btnD.setAttribute('width', '25')
-
-        const cont = document.createElement('div')
-        cont.classList.add('container-items')
-        cont.appendChild(btnC)
-        cont.appendChild(btnD)
-
+        const trash = document.createElement('img')
+        trash.setAttribute('src', 'img/trash-icon.png')
+        trash.setAttribute('width', '25')
         item.textContent = element.text
-
-        item.appendChild(cont)
+        item.appendChild(trash)
         item.setAttribute('id', `${element.id}`)
-
+        if (element.status === true){
+            item.classList.add('complete')
+        }else{
+            item.classList.remove('complete')
+        }
         list.appendChild(item)
     });
-
     syncLS()
-
 }
-
 function clearHTML(){
     while(list.firstChild){
         list.removeChild(list.firstChild)
     }
 }
-
 function deleteItem(e){
     if (e.target.tagName === 'IMG'){
-        const id = Number(e.target.parentElement.parentElement.id)
+        const id = Number(e.target.parentElement.id)
         arrayList = arrayList.filter(element => element.id !== id)
-        createItem(arrayList)
     }
+    if (e.target.tagName === 'LI'){
+        const id = Number(e.target.id)
+        const indice = arrayList.findIndex(element => element.id === id )
+        if (arrayList[indice].status === true){
+            arrayList[indice].status = false
+        }else{
+            arrayList[indice].status = true
+        }
+    }
+    createItem(arrayList)
 }
-
 function syncLS(){
     localStorage.setItem('items', JSON.stringify(arrayList))
 }
-
 function readLS(){
     arrayList = (JSON.parse(localStorage.getItem('items')) || [])
     createItem(arrayList)
