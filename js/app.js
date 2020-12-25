829bc09dc1d855997648c29a53a41d0fc7c8ef6f
Nahuel <69236339+NahueTomas@users.noreply.github.com>
2020-12-25 06:05:07 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 if('serviceWorker' in navigator){
     navigator.serviceWorker.register('./sw.js')
-        .then( registrado => console.log('Se registró de 10 mi rey ', registrado))
-        .catch(error => alert('Falló la instalación ', error))
 }else{
     document.write('Su navegador da asco. Por favor, actualizalo')
 }
--- sw.js
+++ sw.js
@@ -1,8 +1,30 @@
+const nombreCache = 'lista-v1'
+const archivos = [
+    '/',
+    '/index.html',
+    'sw.js',
+    'manifest.json',
+    '/js/app.js',
+    '/js/lista.js',
+    '/css/styles.css',
+    '/img/trash-icon.png',
+    '/img/icons/icon-512.png'
+]
+
 self.addEventListener('install', e=>{
+    e.waitUntil(
+        caches.open(nombreCache)
+            .then(cache => cache.addAll(archivos))
+    )
 })
 
 self.addEventListener('activate', e=>{    
 })
 
-self.addEventListener('fetch', e=>{    
+self.addEventListener('fetch', e=>{
+    console.log('petición')
+    e.respondWith(
+        caches.match(e.request)
+            .then(respuestaCache => respuestaCache)
+    )
 })
